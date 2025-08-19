d52b8b731167d1f7ca9d9173cf983f31fe732c40
Drew Bryan <drewbryan7@gmail.com>
2025-08-19 16:07:17 -0500

    loaded in CSV wit all the amazon books and wrote comments for what certian functions do in pandas

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pandas as pd
+
+df = pd.read_csv("bestsellers.csv")
+
+# head gets the first 5 rows of the spreadsheet
+#print(df.head)
+
+# columns gets the names of every column in the spread sheet
+# print(df.columns)
+
+# shape gets the shape of the spreadsheet or size ex: (500,10)
+# print(df.shape)
+
+# describe Gets the summary statistics for each column
+# print(df.describe())
