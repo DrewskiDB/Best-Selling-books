b03a70087083f2e24802360d4d6d13ba33b0831a
Drew Bryan <drewbryan7@gmail.com>
2025-08-20 17:42:59 -0500

    Created a function that drops the duplicates of books and stored each row into a dictionary

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import pandas as pd
 
-df = pd.read_csv("bestsellers.csv")
+#df = pd.read_csv("bestsellers.csv")
 
 # head gets the first 5 rows of the spreadsheet
-#print(df.head)
-
-# columns gets the names of every column in the spread sheet
-# print(df.columns)
+# print(df.head)
 
 # shape gets the shape of the spreadsheet or size ex: (500,10)
 # print(df.shape)
 
+# columns gets the names of every column in the spread sheet
+# print(df.columns)
+
 # describe Gets the summary statistics for each column
 # print(df.describe())
+
+def df_into_dict(csv):
+    df = pd.read_csv(csv)
+    df_sections = df.drop_duplicates(subset= ['Name', "Author"], keep= 'first')
+    df_sections_dict = df_sections.to_dict(orient = "records")
+
+    for i in df_sections_dict:
+        print(i)
+
+
